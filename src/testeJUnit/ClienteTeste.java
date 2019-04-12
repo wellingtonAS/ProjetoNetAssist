54289aed20bb5574808e8f5682ea2dc97ed05526
wellingtonAS <wellingtonferaifal2012@hotmail.com>
2019-04-12 10:31:23 -0300

    Update ClienteTeste.java
    Implementação da Classe ClienteTeste

--- src/testeJUnit/ClienteTeste.java
+++ src/testeJUnit/ClienteTeste.java
@@ -5,115 +5,90 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ClienteTeste {
+import com.netAssist.Cliente;
+import com.netAssist.Contrato;
+import com.netAssist.Endereco;
+import com.netAssist.Logins;
+import com.netAssist.Tecnico;
 
+public class ClienteTeste {
+	
+	Contrato ct = new Contrato();
+	Cliente c = new Cliente();
+	Tecnico t = new Tecnico();
+	Endereco end = new Endereco();
+	Logins l = new Logins();
 	@Before
 	public void setUp() throws Exception {
+		c.setLogin(l);
+		c.setContrato(ct);
+		c.setAutenticacao("1");
+		c.setTipoConexao("1");
+		c.setLoginAcesso("123");
+		c.setSenhaAcesso("123");
+		c.setVelocidade("5");
+		c.setValorMensal("50");
+		c.setStatusContrato("1");
 	}
 
 	@Test
 	public void testCliente() {
-		fail("Not yet implemented");
+		Cliente c = new Cliente();
 	}
 
 	@Test
 	public void testClienteContratoStringStringEndereco() {
-		fail("Not yet implemented");
+		Cliente c1 = new Cliente(ct, "Maria", "43245648704", end);
 	}
 
 	@Test
 	public void testClienteContrato() {
-		fail("Not yet implemented");
+		c.setContrato(ct);
 	}
 
 	@Test
 	public void testGetLogin() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetLogin() {
-		fail("Not yet implemented");
+		assertEquals(l, c.getLogin());
 	}
 
 	@Test
 	public void testGetContrato() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetContrato() {
-		fail("Not yet implemented");
+		assertEquals(ct, c.getContrato());
 	}
 
 	@Test
 	public void testGetAutenticacao() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetAutenticacao() {
-		fail("Not yet implemented");
+		assertEquals("1", c.getAutenticacao());
 	}
 
 	@Test
 	public void testGetTipoConexao() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetTipoConexao() {
-		fail("Not yet implemented");
+		assertEquals("1", c.getTipoConexao());
 	}
 
 	@Test
 	public void testGetLoginAcesso() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetLoginAcesso() {
-		fail("Not yet implemented");
+		assertEquals("123", c.getLoginAcesso());
 	}
 
 	@Test
 	public void testGetSenhaAcesso() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetSenhaAcesso() {
-		fail("Not yet implemented");
+		assertEquals("123", c.getSenhaAcesso());
 	}
 
 	@Test
 	public void testGetVelocidade() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetVelocidade() {
-		fail("Not yet implemented");
+		assertEquals("5", c.getVelocidade());
 	}
 
 	@Test
 	public void testGetValorMensal() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetValorMensal() {
-		fail("Not yet implemented");
+		assertEquals("50", c.getValorMensal());
 	}
 
 	@Test
 	public void testGetStatusContrato() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetStatusContrato() {
-		fail("Not yet implemented");
+		assertEquals("1", c.getStatusContrato());
 	}
 
 }
