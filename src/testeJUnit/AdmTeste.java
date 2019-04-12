7df7143809b888e3523d407cc45c7eabc19907ad
wellingtonAS <wellingtonferaifal2012@hotmail.com>
2019-04-12 14:57:23 -0300

    Update AdmTeste.java
    Implementação parcial da Classe AdmTeste

--- src/testeJUnit/AdmTeste.java
+++ src/testeJUnit/AdmTeste.java
@@ -2,16 +2,30 @@ package testeJUnit;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-
 import org.junit.Before;
 import org.junit.Test;
 
+import com.netAssist.Adm;
+import com.netAssist.Endereco;
+
 public class AdmTeste {
-	ArrayList admin = new ArrayList();
+	Adm admin = new Adm();
+	Endereco ender = new Endereco();
 	@Before
 	public void setUp() throws Exception {
+		admin.setEnd(ender);
 	}
+
+	@Test
+	public void testAdm() {
+		Adm admin = new Adm();
+	}
+
+	@Test
+	public void testAdmStringStringStringStringStringEndereco() {
+		Adm adm1 = new Adm("admin", "admin", "2", "Teste", "32136537632", ender);
+	}
+
 	@Test
 	public void testCadastrarFuncionario() {
 		//fail("Not yet implemented");
