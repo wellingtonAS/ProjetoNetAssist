67408a744cd6540e7d9d554a05ec50d7e785e5ac
wellingtonAS <wellingtonferaifal2012@hotmail.com>
2019-04-17 09:18:49 -0300

    Update FuncionarioTeste.java
    Atualização da Classe funcionarioTeste

--- src/testeJUnit/FuncionarioTeste.java
+++ src/testeJUnit/FuncionarioTeste.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.netAssist.Endereco;
 import com.netAssist.Funcionario;
 
 public class FuncionarioTeste {
@@ -16,6 +17,8 @@ public class FuncionarioTeste {
 	Funcionario f2 = new Funcionario();
 	@Before
 	public void setUp() throws Exception {
+		Endereco endT = new Endereco();
+		Funcionario fConst = new Funcionario("111", "222", "2", "Jose", "08975612367", endT);
 		fAux.setUsuario("adm");
 		fAux.setSenha("adm");
 		listaF.add(fAux);
@@ -23,20 +26,21 @@ public class FuncionarioTeste {
 		f1.setUsuario("adm");
 		f1.setSenha("adm");
 		f2.setUsuario("admin");
+		fConst.getCargo();
 	}
 
 	@Test
 	public void testEfetuarLoginAutenticando() {
-		assertEquals(2, fAux.efetuarLogin(fAux.getUsuario(), fAux.getSenha(), listaF)); //Caso em que Usuario e Senha est„o corretos	
+		assertEquals(2, fAux.efetuarLogin(fAux.getUsuario(), fAux.getSenha(), listaF)); //Caso em que Usuario e Senha est√£o corretos	
 	}
 	@Test
 	public void testEfetuarLoginErroNaoContinuar() {
-		assertEquals(0, f2.efetuarLogin(f2.getUsuario(), f2.getSenha(), listaF)); //Caso em que Usuario e/ou Senha est„o errados, mas que se deseja tentar novamente		
+		assertEquals(0, f2.efetuarLogin(f2.getUsuario(), f2.getSenha(), listaF)); //Caso em que Usuario e/ou Senha est√£o errados, mas que se deseja tentar novamente		
 	}
 	@Test
 	public void testEfetuarLoginErroContinuar() {
-		assertEquals(1, f2.efetuarLogin(f2.getUsuario(), f2.getSenha(), listaF)); //Caso em que Usuario e/ou Senha est„o errados, mas que NAO se deseja tentar novamente
+		assertEquals(1, f2.efetuarLogin(f2.getUsuario(), f2.getSenha(), listaF)); //Caso em que Usuario e/ou Senha est√£o errados, mas que NAO se deseja tentar novamente
 		
 	}
-
 }
+
